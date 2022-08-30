4078ab20b083e35593f343ea8ea00fdd9713720a
HashBrown12 <hamish.mckenzie21@gmail.com>
2022-08-30 14:21:00 +1200

    Adding UI
    Today I added UI that displays the player's health.

--- Hamish McKenzie - 12DTM Internal 2/Assets/Scripts/GameManager.cs
+++ Hamish McKenzie - 12DTM Internal 2/Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    // Variables
+    private int playerHealth;
+    public TextMeshProUGUI healthText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = 1;
+        healthText.text = "Health: " + playerHealth;
     }
 
     // Update is called once per frame
